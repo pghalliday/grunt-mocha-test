926d02498571a72ed99de5cdbd1e7e5dd6f8f88b
Peter Halliday <pghalliday@gmail.com>
2014-02-23 10:01:47 +0100

    now using cover-child-process to collect coverage data

--- test/tasks/grunt-mocha-test.js
+++ test/tasks/grunt-mocha-test.js
@@ -1,48 +1,16 @@
 /*jshint loopfunc: true */
 
 var expect = require('chai').expect;
-var exec = require('child_process').exec;
+var path = require('path');
 var fs = require('fs');
-
-var mergeCoverageData = function(data) {
-  // we have to reconstruct the the _$jscoverage data
-  // format as it cannot be stringified to JSON with
-  // the additional source property added to arrays
-  if (typeof global._$jscoverage === 'undefined') {
-    global._$jscoverage = {};
-  }
-  var jscoverage = global._$jscoverage;
-  var sourceArrays = data.sourceArrays;
-  var callCounts = data.callCounts;
-  for (var filename in sourceArrays) {
-    var dest = jscoverage[filename];
-    var src = callCounts[filename];
-    src.source = sourceArrays[filename];
-    if (typeof dest === 'undefined') {
-      jscoverage[filename] = src;
-    } else {
-      src.forEach(function(count, index) {
-        if (count !== null) {
-          dest[index] += count;
-        }
-      });
-    }
-  }
-};
+var ChildProcess = require('cover-child-process').ChildProcess;
+var Blanket = require('cover-child-process').Blanket;
+var childProcess = new ChildProcess(new Blanket());
+var gruntExec = 'node ' + path.resolve('node_modules/.bin/grunt');
 
 var execScenario = function(scenario, callback) {
   var scenarioDir = __dirname + '/../scenarios/' + scenario;
-  var child = exec('node ../grunt.js', {cwd: scenarioDir}, function(error, stdout, stderr) {
-    // collect coverage data from file if it exists
-    // this is because the coverage tool does not
-    // really work with child processes so we are
-    // giving it a helping hand
-    var jscoverageFile = scenarioDir + '/jscoverage.json';
-    if (fs.existsSync(jscoverageFile)) {
-      mergeCoverageData(JSON.parse(fs.readFileSync(jscoverageFile)));
-    }
-    callback(error, stdout, stderr);
-  });
+  childProcess.exec(gruntExec, {cwd: scenarioDir}, callback);
 };
 
 describe('grunt-mocha-test', function() {
