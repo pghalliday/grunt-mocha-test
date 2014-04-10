e3ff2e886f152e8d36c724efe8b9a877c4bdafdc
Peter Halliday <pghalliday@gmail.com>
2014-04-10 22:55:18 +0200

    fixing tests

--- test/scenarios/clearRequireCacheOption/testoff.js
+++ test/scenarios/clearRequireCacheOption/testoff.js
@@ -3,6 +3,6 @@ var path = require('path');
 
 describe('check content of require cache contain tasks/mocha.js', function() {
   it('should pass', function() {
-    expect(require.cache).to.have.property(path.resolve(__dirname, '../../../tasks/mocha.js'));
+    expect(require.cache).to.have.property(path.resolve(__dirname, '../../../tasks/mocha-test.js'));
   });
 });
